54bac7025ce3d72b598993152ae6c0005090991a
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-25 23:15:10 +0100

    Create ingestionfromAzuresqlDB.py

---
+++ ingestionfromAzuresqlDB.py
@@ -0,0 +1,12 @@
+jdbc_url = "jdbc:sqlserver://nlazu06003.database.windows.net:1433;database=BasicFit" 
+connection_props = {
+  "user": "sa", 
+  "password": "dgjhgvj", 
+  "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver" }
+
+df_member = spark.read.jdbc(jdbc_url, "dbo.Member", connection_props) 
+df_club = spark.read.jdbc(jdbc_url, "dbo.Club", connection_props) 
+df_visit = spark.read.jdbc(jdbc_url, "dbo.Visit", connection_props)
+bronze_base = "abfss://BF_Bronze@basicfitete.dfs.core.windows.net/" 
+for name, df in [("member", df_member), ("club", df_club), ("visit", df_visit)]:
+  (df.withColumn("ingestion_ts", F.current_timestamp()) .write.format("delta") .mode("overwrite") .save(f"{bronze_base}/{name}"))
