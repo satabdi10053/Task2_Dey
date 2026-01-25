77431b6aac5c0634ce10268645732e23b92a2a44
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-25 15:09:17 +0100

    Update raw.py

--- raw.py
+++ raw.py
@@ -1,1 +1,20 @@
+Lakeflow_BF.BF_Raw.Club [Read the table]
+Lakeflow_BF.BF_Raw.Member
+Lakeflow_BF.BF_Raw.Visit
+
+df = spark.table("Lakeflow_BF.BF_Raw.Club") [load table in Dataframe]
+df = spark.table("Lakeflow_BF.BF_Raw.Member")
+df = spark.table("Lakeflow_BF.BF_Raw.Visit")
+
+  df.write .mode("overwrite")
+ .partitionBy("ingestion_date")
+ .parquet("raw@basicfitete.dfs.core.windows.net/BF_Raw/Club/ingestion_date=2026-01-25/")
+
+df.write .mode("overwrite")
+ .partitionBy("ingestion_date")
+ .parquet("raw@basicfitete.dfs.core.windows.net/BF_Raw/Visit/ingestion_date=2026-01-25/")
+
+df.write .mode("overwrite")
+ .partitionBy("ingestion_date")
+ .parquet("raw@basicfitete.dfs.core.windows.net/BF_Raw/member/ingestion_date=2026-01-25/")
 
