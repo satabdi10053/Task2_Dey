724145e02860b79f7c7373d1649c667f9b2732a2
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-25 22:32:01 +0100

    Update BronzeLayerADLS.py

--- BronzeLayerADLS.py
+++ BronzeLayerADLS.py
@@ -5,3 +5,11 @@ df_visit = spark.table("Lakeflow_BF.BF_Bronze.visit")
 df_club.write.format("delta") \ 
 .mode("append") \ 
 .save("abfss://BF_Bronze@basicfitete.dfs.core.windows.net/club")
+
+df_member.write.format("delta") \ 
+.mode("append") \ 
+.save("abfss://BF_Bronze@basicfitete.dfs.core.windows.net/member")
+
+df_visit.write.format("delta") \ 
+.mode("append") \ 
+.save("abfss://BF_Bronze@basicfitete.dfs.core.windows.net/visit")
