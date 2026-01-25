9970f720a2aacd73606e5875037baafd33a6922b
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-25 22:30:05 +0100

    Update and rename BronzeLayer.py to BronzeLayerADLS.py

---
+++ BronzeLayerADLS.py
@@ -0,0 +1,7 @@
+df_club = spark.table("Lakeflow_BF.BF_Bronze.club")
+df_member = spark.table("Lakeflow_BF.BF_Bronze.member") 
+df_visit = spark.table("Lakeflow_BF.BF_Bronze.visit")
+
+df_club.write.format("delta") \ 
+.mode("append") \ 
+.save("abfss://BF_Bronze@basicfitete.dfs.core.windows.net/club")
