9ed648a0a26b8aab3e464350e8f7cb4ce77c6d61
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 00:27:08 +0100

    Update Silver_club.py

--- Silver_club.py
+++ Silver_club.py
@@ -5,6 +5,9 @@ df_club.withcolumnRenamed("_rescued_data", "unexpected_Data").display()\
 df_club=df_club.withcolumn("last_modification_date_utc",to_utc_timestamp(col("last_modification_date"), "UTC"))
 df_club.write.format("delta").mode("äppend").save("abfss://BF_Silver@basicfitete.dfs.core.windows.net/club")
 
+%SQL
+CREATE TABLE Lakeflow_BF.BF_Bronze.club USING DELTA LOCATION 'abfss://BF_Bronze@basicfitete.dfs.core.windows.net/club';
+
 
 
 
