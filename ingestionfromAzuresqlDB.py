585c5bba9462cf7429df160f681b255e788999a9
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-26 10:21:17 +0100

    Update ingestionfromAzuresqlDB.py

--- ingestionfromAzuresqlDB.py
+++ ingestionfromAzuresqlDB.py
@@ -1,4 +1,4 @@
-Ingestion from Azure sql DB and stored in ADLS Gen2
+Ingestion from Azure sql DB and stored in ADLS Gen2 RAW
 
 jdbc_url = "jdbc:sqlserver://nlazu06003.database.windows.net:1433;database=BasicFit" 
 connection_props = {
@@ -9,13 +9,13 @@ connection_props = {
 df_member = spark.read.jdbc(jdbc_url, "dbo.Member", connection_props) 
 df_club = spark.read.jdbc(jdbc_url, "dbo.Club", connection_props) 
 df_visit = spark.read.jdbc(jdbc_url, "dbo.Visit", connection_props)
-bronze_base = "abfss://BF_Bronze@basicfitete.dfs.core.windows.net/" 
+raw_base = "abfss://BF_Raw@basicfitete.dfs.core.windows.net/" 
 for name, df in [("member", df_member), ("club", df_club), ("visit", df_visit)]:
-  (df.withColumn("ingestion_ts", F.current_timestamp()) .write.format("delta") .mode("append") .save(f"{bronze_base}/{name}"))
+  (df.withColumn("ingestion_ts", F.current_timestamp()) .write.format("delta") .mode("append") .save(f"{raw_base}/{name}"))
 
 
 
 %SQL [Register tables in Unity Catalog]
-CREATE TABLE Lakeflow_BF.BF_Bronze.Member USING DELTA LOCATION 'abfss://BF_Bronze@basicfitete.dfs.core.windows.net/member';
-CREATE TABLE Lakeflow_BF.BF_Bronze.club USING DELTA LOCATION 'abfss://BF_Bronze@basicfitete.dfs.core.windows.net/club';
-CREATE TABLE Lakeflow_BF.BF_Bronze.visit USING DELTA LOCATION 'abfss://BF_Bronze@basicfitete.dfs.core.windows.net/visit';
+CREATE TABLE Lakeflow_BF.BF_Raw.Member USING DELTA LOCATION 'abfss://BF_Raw@basicfitete.dfs.core.windows.net/member';
+CREATE TABLE Lakeflow_BF.BF_Raw.club USING DELTA LOCATION 'abfss://BF_Raw@basicfitete.dfs.core.windows.net/club';
+CREATE TABLE Lakeflow_BF.BF_Raw.visit USING DELTA LOCATION 'abfss://BF_Raw@basicfitete.dfs.core.windows.net/visit';
