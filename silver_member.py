fd79dcda6442c15011d6534c52e30de28777fdf6
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 00:28:33 +0100

    Update silver_member.py

--- silver_member.py
+++ silver_member.py
@@ -4,3 +4,6 @@ df_member=spark.read.format("delta")\
 df_member.withcolumnRenamed("_rescued_data", "unexpected_Data").display()\ 
 df_member=df_member.withcolumn("last_modification_date_utc",to_utc_timestamp(col("last_modification_date"), "UTC"))
 df_member.write.format("delta").mode("äppend").save("abfss://BF_Silver@basicfitete.dfs.core.windows.net/member")
+
+%SQL
+CREATE TABLE Lakeflow_BF.BF_Silver.Member USING DELTA LOCATION 'abfss://BF_Silver@basicfitete.dfs.core.windows.net/member';
