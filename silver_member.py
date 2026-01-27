280a57f19d4ce75176677f37a41929b082173b49
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 19:21:39 +0100

    Update silver_member.py

--- silver_member.py
+++ silver_member.py
@@ -9,3 +9,5 @@ df_member=df_member.dropDuplicates(subset['people_id'])
 
 %SQL
 CREATE TABLE Lakeflow_BF.BF_Silver.Member USING DELTA LOCATION 'abfss://BF_Silver@basicfitete.dfs.core.windows.net/member';
+A
+MERGE INTO silver.member AS tgt USING bronze.member AS src ON tgt.member_id = src.member_id WHEN MATCHED AND src.SYS_CHANGE_OPERATION = 'D' THEN DELETE WHEN MATCHED AND src.SYS_CHANGE_OPERATION <> 'D' THEN UPDATE SET * WHEN NOT MATCHED AND src.SYS_CHANGE_OPERATION <> 'D' THEN INSERT *;
