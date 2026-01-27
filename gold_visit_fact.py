d8ebb80085c26a30f5947d7a31fc950764d01f62
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 19:50:45 +0100

    Update gold_visit_fact.py

--- gold_visit_fact.py
+++ gold_visit_fact.py
@@ -3,8 +3,9 @@ from pyspark.sql.types import *
 
 df_visit.spark.sql("select * from Lakeflow_BF.BF_Silver.visit")
 
+df_dim_member=spark.sql(select dim_people_key,people_id as dim_people_id from Lakeflow_BF.BF_gold.dim_Member
 
-from pyspark.sql.functions import col, to_date 
+
 fact_visit = ( df_visit.alias("v") 
               .join(dim_member.alias("m"), "people_id") 
               .join(dim_club.alias("c"), "club_id") 
