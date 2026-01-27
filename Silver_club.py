c35352a45f4aa94c37acd2c109edf0298f617bce
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 10:00:29 +0100

    Update Silver_club.py

--- Silver_club.py
+++ Silver_club.py
@@ -2,8 +2,12 @@ df_club=spark.read.format("delta")\
 .load("abfss://BF_Bronze@basicfitete.dfs.core.windows.net/club")\
 .display(df_club)  [autoloader automatically creates extra schema that is "rescued_data" or I am saying it as unexpected data]
 df_club.withcolumnRenamed("_rescued_data", "unexpected_Data").display()\ 
-df_club=df_club.withcolumn("last_modification_date_utc",to_utc_timestamp(col("last_modification_date"), "UTC"))
+df_club=df_club.withcolumn("last_modification_date_utc",to_utc_timestamp(col("last_modification_date"), "UTC")) [convert timestamp]
 df_club.write.format("delta").mode("äppend").save("abfss://BF_Silver@basicfitete.dfs.core.windows.net/club")
+
+
+
+
 
 %SQL
 CREATE TABLE Lakeflow_BF.BF_Silver.club USING DELTA LOCATION 'abfss://BF_Silver@basicfitete.dfs.core.windows.net/club';
