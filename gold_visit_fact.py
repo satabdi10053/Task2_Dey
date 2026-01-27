8a29d86b9733f3b18d77aa9eb2e46230da2d2b3e
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 21:05:56 +0100

    Update gold_visit_fact.py

--- gold_visit_fact.py
+++ gold_visit_fact.py
@@ -11,4 +11,9 @@ df_dim_club=spark.sql("select dim_club_key,club_id as dim_club_id from Lakeflow_
 df_fact_visit =df.join(df_dim_member,df['people_id']==df_dim_member['dim_people_id'],how=left).join(df_dim_club,df['club_id']==df_dim_club['dim_club_id'],how=left)
 df_fact_visit_new=df_fact_visit.drop('dim_people_id', 'dim_club_id')
 
-dim_club.write.format("delta").mode("append").save("abfss://BF_Gold@basicfitete.dfs.core.windows.net/visit")
+fact_visit.write.format("delta").mode("append").save("abfss://BF_Gold@basicfitete.dfs.core.windows.net/visit")
+
+%SQL
+CREATE TABLE Lakeflow_BF.BF_gold.fact_visit USING DELTA LOCATION 'abfss://BF_Gold@basicfitete.dfs.core.windows.net/visit';
+
+
