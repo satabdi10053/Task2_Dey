77eab11905ad707d1310c48d40b280ba971440ac
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 20:04:37 +0100

    Update gold_dim_member.py

--- gold_dim_member.py
+++ gold_dim_member.py
@@ -6,4 +6,9 @@ dim_member = ( df_member .select( F.monotonically_increasing_id().alias("Dim_peo
 dim_member.write.format("delta").mode("append").save("abfss://BF_gold@basicfitete.dfs.core.windows.net/member")
 
 
+%SQL
+
+CREATE TABLE Lakeflow_BF.BF_gold.dim_Member USING DELTA LOCATION 'abfss://BF_Silver@basicfitete.dfs.core.windows.net/member';
+
+
 
