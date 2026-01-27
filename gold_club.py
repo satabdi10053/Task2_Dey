50539c34726ee6f4fd6205de5ae2932cf93cf1af
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 13:01:08 +0100

    Update gold_club.py

--- gold_club.py
+++ gold_club.py
@@ -1,3 +1,6 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
 df.spark.sql("select * from Lakeflow_BF.BF_Silver.club")
 
 dim_club = ( df_club .select( F.monotonically_increasing_id().alias("club_sk"), "club_id", "club_name", "city", "state", "country", "opened_date" ) )
