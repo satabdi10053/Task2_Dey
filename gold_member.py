65c167f48ba5d9592c181ef9b788f83214b5cba3
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 17:38:30 +0100

    Update gold_member.py

--- gold_member.py
+++ gold_member.py
@@ -6,3 +6,5 @@ df_member= spark.sql("select * from Lakeflow_BF.BF_Silver.member")
 dim_member = ( df_member .select( F.monotonically_increasing_id().alias("Dim_people_key"), "people_id", "people_age", "people_membership_type", "last_modification_date" ) )
 
 dim_member.write.format("delta").mode("append").save("abfss://BF_gold@basicfitete.dfs.core.windows.net/member")
+
+
