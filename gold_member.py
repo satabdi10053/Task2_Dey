6d0544772b92ff9bb22d8e2e3074690a920668d5
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 13:19:11 +0100

    Update gold_member.py

--- gold_member.py
+++ gold_member.py
@@ -4,3 +4,5 @@ from pyspark.sql.types import *
 df_member= spark.sql("select * from Lakeflow_BF.BF_Silver.member")
 
 dim_member = ( df_member .select( F.monotonically_increasing_id().alias("Dim_people_key"), "people_id", "people_age", "people_membership_type", "last_modification_date" ) )
+
+dim_member.write.format("delta").mode("append").save("abfss://BF_gold@basicfitete.dfs.core.windows.net/member")
