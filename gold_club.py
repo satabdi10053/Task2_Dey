71e2c864117fc5db6543789b544561da73dd6b5b
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-01-27 13:20:58 +0100

    Update gold_club.py

--- gold_club.py
+++ gold_club.py
@@ -5,4 +5,4 @@ df_club.spark.sql("select * from Lakeflow_BF.BF_Silver.club")
 
 dim_club = ( df_club .select( F.monotonically_increasing_id().alias("club_sk"), "club_id", "club_name", "city", "state", "country", "opened_date" ) )
 
-dim_club.write.format("delta").mode("append").save("abfss://BF_Silver@basicfitete.dfs.core.windows.net/club")
+dim_club.write.format("delta").mode("append").save("abfss://BF_Gold@basicfitete.dfs.core.windows.net/club")
